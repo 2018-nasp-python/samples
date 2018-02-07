81ab50eda90b6d4ebd6803e4238fdf1aa6a2d58c
Thomas Lane <tlane@bcit.ca>
2018-02-07 08:32:36 -0800

    adds file read / write examples

---
+++ file_io/01_tex_file_writing.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+"""Writing Text to a File
+"""
+import os
+
+# Get current directory of script
+script_dir = os.path.dirname(__file__)
+
+# Use a file path relative to the executing file so that we always know where 
+# the ouput is going
+over_write_path = os.path.join(script_dir, 'example_data', 'over_write_file.txt')
+append_path = os.path.join(script_dir, 'example_data', 'append_file.txt')
+
+# see https://docs.python.org/3/library/functions.html#open for details of opening modes
+over_write_file = open(over_write_path, 'w')
+append_file = open(append_path, 'a')
+
+text = ""
+print("Write to files: Part 1")
+while True:
+    text = input("Enter input or 'EOF' to quit: ")
+    if text == "EOF":
+        break
+
+    #Uses format string method for ouput see:
+    # https://docs.python.org/3/tutorial/inputoutput.html#fancier-output-formatting
+    over_write_file.write("{}\n".format(text))
+    append_file.write("{}\n".format(text))
+
+#Close files,  flushing them to disk
+over_write_file.close()
+append_file.close()
+    
+over_write_file = open(over_write_path, 'w')
+append_file = open(append_path, 'a')
+text = ""
+print("Write to files: Part 2 - no newline")
+while True:
+    text = input("Enter input or 'EOF' to quit: ")
+    if text == "EOF":
+        break
+    over_write_file.write(text)
+    append_file.write(text)
+
+#Close files,  flushing them to disk
+over_write_file.close()
+append_file.close()
+
+
---
+++ file_io/03_reading_file.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+import os
+# Get current directory of script
+script_dir = os.path.dirname(__file__)
+
+# Use a file path relative to the executing file so that we always know where 
+# the input is is coming form 
+man_page_path = os.path.join(script_dir, 'example_data', 'uname.txt')
+
+man_page_file = open(man_page_path)
+whole_file_as_string = man_page_file.read()
+man_page_file.close()
+print(whole_file_as_string)
+
+man_page_file = open(man_page_path)
+whole_file_line_list = man_page_file.readlines()
+print(whole_file_line_list)
+man_page_file.close()
+
+man_page_file = open(man_page_path)
+first_line = man_page_file.readline()
+print(first_line)
+man_page_file.close()
+
+man_page_file = open(man_page_path)
+for line in man_page_file:
+    if line == "AUTHOR\n":
+        author = man_page_file.readline().strip("Written by ").rstrip(".\n")
+man_page_file.close()
