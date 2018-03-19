d58278fbf50c6dff5cb3fd4b98677ec10764ecee
Thomas Lane <tlane@bcit.ca>
2018-03-19 11:43:23 -0700

    use argument list rather than sys.argv in show arguments

--- arguments/argparse_01.py
+++ arguments/argparse_01.py
@@ -6,8 +6,9 @@ def show_args(argument_list):
     parser = argparse.ArgumentParser(description="Demonstration argument parser")
     parser.add_argument("command", help="command argument")
     parser.add_argument("-d", "--debug", help="debug switch", action="store_true")
-    arguments = parser.parse_args()
+    arguments = parser.parse_args(argument_list))
     print(arguments.command)
     print(arguments.debug)
 if __name__ == '__main__':
     show_args(sys.argv)
+    show_args(['list', 'of', 'stuff'])
